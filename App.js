7ddaad7847d6f4378b0fff990d6b80c2b1f77fcd
sar653 <sarva653@gmail.com>
2023-10-25 13:30:28 +0530

    newly added

--- App.js
+++ App.js
@@ -1,15 +1,85 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+//const heading= React.createElement(
+//  "h1",{id:"heading"
+//},"nam6aste react")
+
+const Header = () => {
+  return (
+    <div className="header">
+      <div className="logocontainer">
+        <img
+          className="logo"
+          src="https://www.logodesign.net/logo/smoking-burger-with-lettuce-3624ld.png"
+        />
+      </div>
+      <div className="nav-items">
+        <ul>
+          <li>home</li>
+          <li>aboutus</li>
+          <li>contact us</li>
+          <li>cart</li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+const RestaurantCard = (props) => {
+  const { recipe,resName}=props
+  return (
+    <div className="res-card" style={{ backgroundColor: "#f0f0f0" }}>
+      <img className="res-logo" alt="res-logo" src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_208,h_208,c_fit/druwjzmfmz7qvepq3bkr"/>
+      <h3>{props.recipe}</h3>
+      <h4>{props.resName}</h4>
+      <h4>4.4stars</h4>
+      <h4>38 minutes</h4>
+    </div>
+  );
+};
+const Body = () => {
+  return (
+    <div className="body">
+      <div className="search"> search</div>
+      <div className="res-container">
+        <RestaurantCard resName="baghdad" recipe="briyani"/>
+        <RestaurantCard resName="hyderabad" recipe="briyani"/>
+        <RestaurantCard />
+        
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+
+      </div>
+    </div>
+  );
+};
+
+const Applayout = () => {
+  return (
+    <div className="app">
+      
+      <Header />
+      <Body />
+    </div>
+  );
+};
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<Applayout />);
 //<div>
 //<div>
-  //  <h1>im from h1 tag</h1>
- // <h2>im from h2 tag</h2>
- //   </div> <div> 
- //<h1>im from h1 tag</h1>
- //<h2>im from h2 tag
- //</h2>
- //</div>
+//  <h1>im from h1 tag</h1>
+// <h2>im from h2 tag</h2>
+//   </div> <div>
+//<h1>im from h1 tag</h1>
+//<h2>im from h2 tag
+//</h2>
+//</div>
 //</div>
 
-const parent=React.createElement
+/*const parent=React.createElement
 ("div",{id:"parent"},
 React.createElement("div",{id:"child"},
 [React.createElement("h1",{},"im from h1 tag"),
@@ -25,10 +95,7 @@ console.log(parent)
 
 const root= ReactDOM.createRoot(document.getElementById("root"))
 
-root.render(parent)
-
-
-
+root.render(parent)*/
 
 //const heading=React.createElement("h1",{id:"heading"},"hello world from react");
 //const root= ReactDOM.createRoot(document.getElementById("root"))
